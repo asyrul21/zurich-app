54fb3d8c861e4605bf3c21bd49c72baeeaa79eb5
asyrul21 <asyrulhafetzy.21@gmail.com>
2023-12-15 00:01:52 +0800

    users data recursive retrieval implementation

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,6 +2,18 @@ import { maskEmailString } from "@/app/utils";
 
 const BASE_URL = process.env.API_BASE_URL;
 
+const recursiveGetUsers: (
+  currentPage: number,
+  pageData: unknown[],
+) => Promise<unknown[]> = async (currentPage: number, pageData: unknown[]) => {
+  const response = await fetch(`${BASE_URL}/users?page=${currentPage}`);
+  const result = await response.json();
+  if (result.page === result.total_pages) {
+    return [...pageData, ...result.data];
+  }
+  return await recursiveGetUsers(result.page + 1, result.data);
+};
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const maskEmailParamStr =
@@ -9,15 +21,8 @@ export async function GET(request: Request) {
   const showEmailsFor = JSON.parse(maskEmailParamStr);
 
   try {
-    // get total document ocunt
-    const initResponse = await fetch(`${BASE_URL}/users`);
-    const initResult = await initResponse.json();
-    const total = initResult.total;
+    let resultData = await recursiveGetUsers(1, []);
 
-    const response = await fetch(`${BASE_URL}/users?per_page=${total}`);
-    const result = await response.json();
-
-    let resultData = result.data;
     resultData = resultData.filter((d: any) => {
       const firstCharFirstName = d.first_name.split("")[0];
       const firstCharLastName = d.last_name.split("")[0];
@@ -37,7 +42,6 @@ export async function GET(request: Request) {
     });
 
     const returnObj = {
-      ...result,
       data: resultData,
     };
 
